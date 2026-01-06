1e35635f36373e79ad0e04ca2d100da7f3a0e8e6
atomsprint <atoms_print@studyatoms.com>
2026-01-05 16:09:13 -0800

    Add files via upload
    Initial commit

---
+++ IzaKamakuraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IzaKamakuraApp
+//
+//  Created by Hide I on 2026/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
