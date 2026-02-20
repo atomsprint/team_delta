8aba024511f8967d82e00f4e8e2b0fd1ad80c4b4
Haruto <ide.forcontact@gmail.com>
2026-02-20 22:29:35 +0900

    建長寺と日本文化を追加しました

---
+++ IzaKamakuraApp/TenthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TenthViewController.swift
+//  IzaKamakuraApp
+//
+//  Created by Hide I on 2026/02/20.
+//
+
+import UIKit
+
+class TenthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
