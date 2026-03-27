9f7c522d190081540d0f598ace6f571d254f83b0
Haruto <ide.forcontact@gmail.com>
2026-03-28 08:50:24 +0900

    色々変えたよ

---
+++ IzaKamakuraApp/EleventhViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EleventhViewController.swift
+//  IzaKamakuraApp
+//
+//  Created by Hide I on 2026/03/27.
+//
+
+import UIKit
+
+class EleventhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
